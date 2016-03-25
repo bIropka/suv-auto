ed7372e5d13d76175e338fa481a2fea9081c0ad3
bIropka <biropka@gmail.com>
2016-03-25 17:18:25 +0200

    v 0.4

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
 
-    $('.block-choice .select-image').click(function() {
+    $('.select-image').click(function() {
+
         var selectorId = $(this).index();
-        $(this).find('ul').fadeToggle();
-        if(selectorId == 0) {
-            $('.block-choice .select-image').eq(1).find('ul').fadeOut(100);
-        } else if(selectorId == 1) {
-            $('.block-choice .select-image').eq(0).find('ul').fadeOut(100);
-        }
+        $('.select-image').each(function(index, element) {
+            if(index == selectorId){
+                $(element).find('ul').fadeToggle();
+            } else {
+                $(element).find('ul').fadeOut(100);
+            }
+        });
     });
 
-    $('.block-choice .select-image ul li').click(function() {
+    $('.select-image ul li').click(function() {
         var selectorId = $(this).parents('.select-image').index();
         var productName = $(this).html();
         var productId = $(this).index();
@@ -27,12 +29,35 @@ $(document).ready(function () {
     });
 
     $('.product-amount-controls .control').click(function() {
-        var currentAmount = $(this).siblings('label').find('.input-amount').val();
+        var thisInput = $(this).siblings('label').find('.input-amount');
+        var currentAmount = parseInt($(thisInput).val());
+        var amountImage = $(this).parents('.product-amount').find('.current-amount');
         console.log(currentAmount);
         if($(this).hasClass('control-add')) {
-            //currentAmount
+            $(thisInput).val(++currentAmount);
+            $(amountImage).html($(thisInput).val());
+        } else if($(this).hasClass('control-remove')) {
+            if(currentAmount > 0) {
+                $(thisInput).val(--currentAmount);
+                $(amountImage).html($(thisInput).val());
+            }
         }
     });
+
+    $('.pages-nav ul li').click(function() {
+        if(!$(this).hasClass('active') && !$(this).hasClass('three-dot')){
+            $('.pages-nav ul li').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+
+    $('.filter-oils-2 li').click(function() {
+        if(!$(this).hasClass('active')){
+            $('.filter-oils-2 li').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+
 
     $('.slider').slick({
         autoplay: false,
